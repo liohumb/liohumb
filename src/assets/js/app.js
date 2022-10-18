aa55bda61edf48e4364780c701b7c740c7fab28f
liohumb <helloliohumb@gmail.com>
2022-10-18 14:10:12 +0200

    aos correction

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -16,4 +16,15 @@ import AOS from 'aos'
 import 'aos/dist/aos.css'
 
 // Animate On Scroll
-AOS.init()
+AOS.init({
+    offset: 60,
+    once: false,
+    disable: 'phone',
+    duration: 300,
+    initClassName: 'aos-init',
+    animatedClassName: 'aos-animate',
+    useClassNames: false,
+    disableMutationObserver: false,
+    debounceDelay: 50,
+    throttleDelay: 99
+})
